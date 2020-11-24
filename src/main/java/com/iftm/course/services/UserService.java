b4e63273ee173e6bac1d19e69da0d11adbbe0388
Andre Vilela <andrevibegoogle@hotmail.com>
2020-11-24 20:27:17 -0300

    Exception handling - update

--- src/main/java/com/iftm/course/services/UserService.java
+++ src/main/java/com/iftm/course/services/UserService.java
@@ -45,9 +45,14 @@ public class UserService {
 	}
 	
 	public User update(Long id, User obj) {
-		User entity = repository.getOne(id);
-		updateData(entity, obj);
-		return repository.save(entity);
+		try {
+			User entity = repository.getOne(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+		} catch (Exception e) {
+			throw new ResourceNotFoundException(id);
+		}
+		
 	}
 
 	private void updateData(User entity, User obj) {
