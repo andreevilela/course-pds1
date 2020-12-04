26578514c9e917350a25e45ea3d0d78fcd637c54
Andre Vilela <andrevibegoogle@hotmail.com>
2020-12-04 20:57:36 -0300

    Challenges: category CRUD, product CRUD + CategoryDTO, ProductDTO

--- src/main/java/com/iftm/course/services/ProductService.java
+++ src/main/java/com/iftm/course/services/ProductService.java
@@ -2,12 +2,20 @@ package com.iftm.course.services;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
+
+import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
+import com.iftm.course.dto.ProductDTO;
 import com.iftm.course.entities.Product;
 import com.iftm.course.repositories.ProductRepository;
+import com.iftm.course.services.exceptions.DatabaseException;
+import com.iftm.course.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class ProductService {
@@ -15,14 +23,53 @@ public class ProductService {
 	@Autowired
 	private ProductRepository repository;
 	
-	public List<Product> findAll() {
-		return repository.findAll();
+	public List<ProductDTO> findAll() {
+		List<Product> list = repository.findAll();
+		return list.stream().map(e -> new ProductDTO(e)).collect(Collectors.toList());
 		
 	}
 	
-	public Product findById(Long id) {
+	public ProductDTO findById(Long id) {
 		Optional<Product> obj = repository.findById(id);
-		return obj.get();
+		Product entity =  obj.get();
+		return new ProductDTO(entity);
+	}
+	
+	public ProductDTO insert(ProductDTO dto) {
+		Product entity = dto.toEntity();
+		entity =  repository.save(entity);
+		return new ProductDTO(entity);
+	}
+	
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
+		
+	}
+	
+	@Transactional
+	public ProductDTO update(Long id, ProductDTO dto) {
+		try {
+			Product entity = repository.getOne(id);
+			updateData(entity, dto);
+			entity =  repository.save(entity);
+			return new ProductDTO(entity);
+		} catch (Exception e) {
+			throw new ResourceNotFoundException(id);
+		}
+		
+	}
+
+	private void updateData(Product entity, ProductDTO dto) {
+		entity.setName(dto.getName());
+		entity.setDescription(dto.getDescription());
+		entity.setPrice(dto.getPrice());
+		entity.setImgURL(dto.getImgURL());
 	}
 
 }
